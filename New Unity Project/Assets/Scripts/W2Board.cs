259523e3822d264a99a5266021c6d491e49073e4
Juno Morrow <sabotaione@gmail.com>
2020-02-13 18:19:53 -0500

    Initial commit

---
+++ New Unity Project/Assets/Scripts/W2Board.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W2Board : MonoBehaviour
+{
+    public GameObject player;
+    //Vector3 initPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            player.transform.position += new Vector3(1, 0, 0);
+        }
+    }
+}
