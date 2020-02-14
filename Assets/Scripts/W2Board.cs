9faf4686b8e44eb0d5cd8a44ec5d98f5a48dd93f
Juno Morrow <sabotaione@gmail.com>
2020-02-13 20:17:15 -0500

    did week 3 stuff

--- Assets/Scripts/W2Board.cs
+++ Assets/Scripts/W2Board.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class W2Board : MonoBehaviour
 {
     public GameObject player;
-    //Vector3 initPos;
+    Vector3 initPos;
+    public Transform[] hazard;
     
     // Start is called before the first frame update
     void Start()
     {
-        //initPos = player.transform.position;
+        initPos = player.transform.position;
     }
 
     // Update is called once per frame
@@ -24,5 +25,30 @@ public class W2Board : MonoBehaviour
         {
             player.transform.position += new Vector3(1, 0, 0);
         }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            player.transform.position += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            player.transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            player.transform.position += new Vector3(0, -1, 0);
+        }
+
+        for (int i = 0; i < hazard.Length; i++)
+        {
+            if (hazard[i].position == player.transform.position)
+            {
+                player.transform.position = initPos;
+            }
+        }
+        
     }
 }
