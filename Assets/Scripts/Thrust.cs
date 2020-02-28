4d4a53564d14ca3df9e9ea3a46440a73d4456e1b
Juno Morrow <sabotaione@gmail.com>
2020-02-27 20:18:25 -0500

    added week 5 stuff with physics intro

---
+++ Assets/Scripts/Thrust.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject victim;
+           // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * 5f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * 5f);
+        }
+    }
+    void OnCollisionEnter(Collision myCollision)
+    {
+        if (myCollision.gameObject == victim)
+        {
+            Destroy(victim);
+        }
+        //print("lol collision");
+    }
+}
