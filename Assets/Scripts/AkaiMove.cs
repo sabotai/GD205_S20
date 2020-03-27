e59687f425cfa94eb189ed37b497a9a647e5108f
Juno AM <sabotaione@gmail.com>
2020-03-26 20:03:56 -0400

    added animation state w8 content and another raycasting demo

---
+++ Assets/Scripts/AkaiMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AkaiMove : MonoBehaviour
+{
+
+	Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	myAnim.SetInteger("MoveState", 0);
+        if (Input.GetKey(KeyCode.W)){
+        	myAnim.SetInteger("MoveState", 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+        	myAnim.SetInteger("MoveState", -1);
+        }
+    }
+}
